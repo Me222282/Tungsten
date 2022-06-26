3571976e434599b8a78d17028fa9f5d29da1a90d
Zene <oinkymail3@gmail.com>
2022-06-26 17:24:01 +0100

    Added project Compiled

---
+++ Compiled/Function.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiled
+{
+    public class Function
+    {
+        public int Id { get; }
+
+        public Instruction[] Code { get; }
+    }
+}
---
+++ Compiled/Instruction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiled
+{
+    public class Instruction
+    {
+        private Instruction(InstType type, IInstInfo info)
+        {
+            Type = type;
+            Info = info;
+        }
+
+        public InstType Type { get; }
+        public IInstInfo Info { get; }
+
+        public static Instruction Call(Function function, ArgInfo[] arguments, ArgInfo @return)
+        {
+            return new Instruction(InstType.FunctionCall, new FuncInfo(function, arguments, @return));
+        }
+        public static Instruction MemAlloc(int size)
+        {
+            return new Instruction(InstType.Alloc, new AllocInfo(false, size));
+        }
+        public static Instruction StackAlloc(int size)
+        {
+            return new Instruction(InstType.Alloc, new AllocInfo(true, size));
+        }
+    }
+}
